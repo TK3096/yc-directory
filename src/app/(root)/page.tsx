1bac092e3fca7a2028487f2f162913a3953c06b7
tongthanaphon <tongthanaphon.tk@gmail.com>
2025-03-03 23:55:32 +0700

    feat: search

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -13,9 +13,10 @@ export default async function Home(props: Props) {
   const { searchParams } = props
 
   const query = (await searchParams).query
+  const params = { search: query || null }
 
   // const posts = await client.fetch(STARTUPS_QUERY)
-  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY })
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params })
 
   return (
     <>
